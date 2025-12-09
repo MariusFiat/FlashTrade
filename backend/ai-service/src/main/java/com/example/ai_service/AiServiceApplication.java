d493002db3203f0640e66c6178daee940667f252
Alexandru <Burada Alexandru>
2025-12-09 20:49:48 +0200

    ai part still in progress

--- backend/ai-service/src/main/java/com/example/ai_service/AiServiceApplication.java
+++ backend/ai-service/src/main/java/com/example/ai_service/AiServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AiServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AiServiceApplication.class, args);
+		SpringApplication.run(AiServiceApplication.class, args); //Alex a aruncat o privire doar momentan :))
 	}
 
 }
